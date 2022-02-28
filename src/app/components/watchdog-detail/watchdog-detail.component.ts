05ecf32ebcdce904cccd584ac2662be005d54244
davidszorad <david.szorad@live.com>
2022-02-28 23:43:45 +0100

    Add detail

--- src/app/components/watchdog-detail/watchdog-detail.component.ts
+++ src/app/components/watchdog-detail/watchdog-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Watchdog } from 'src/app/models/watchdog';
+import { WatchdogSingleQuery } from 'src/app/models/watchdog-single-query';
+import { WatchdogService } from 'src/app/services/watchdog.service';
 
 @Component({
   selector: 'app-watchdog-detail',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./watchdog-detail.component.css']
 })
 export class WatchdogDetailComponent implements OnInit {
+  isLoading: boolean = true;
+  title: string = '';
+  watchdog: Watchdog = {} as Watchdog;
+  query: WatchdogSingleQuery = { publicId: '' };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private wdService: WatchdogService,
+    private sanitizer: DomSanitizer) {
+    route.params.subscribe(p => {
+      this.query.publicId = p['url'];
+    });
+  }
 
   ngOnInit(): void {
+    this.wdService.getSingle(this.query)
+      .subscribe(
+        result => {
+          if (result.allCount === 1) {
+            this.watchdog = result.items[0];
+            this.title = this.watchdog.name;
+          }
+          this.isLoading = false;
+          this.scrollToTop();
+        },
+        err => {
+          if (err.status == 404) {
+            this.router.navigate(['/']);
+          }
+        });
+  }
+
+  scrollToTop() {
+    (function smoothscroll() {
+      window.scrollTo(0, 0);
+    })();
   }
 
 }
